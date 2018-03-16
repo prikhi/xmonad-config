10796bc07179dfc5b842801b2d395b1568dac6c3
Pavan Rikhi <pavan.rikhi@gmail.com>
2018-03-16 04:52:18 -0400

    Add Replacements for Default Keybindings

--- src/xmonad.hs
+++ src/xmonad.hs
@@ -8,9 +8,11 @@ import XMonad.Util.Run (spawnPipe)
 
 import Data.Monoid (Endo)
 import Flow
+import System.Exit (exitSuccess)
 import System.IO (Handle, hPutStrLn)
 
 import qualified Data.Map as Map
+import qualified XMonad.StackSet as W
 
 
 main :: IO ()
@@ -39,7 +41,7 @@ myConfig = do
         , manageHook =
             myManageHook
         , keys =
-            myKeys <+> keys def
+            myKeys
         }
         |> docks
         |> return
@@ -97,12 +99,96 @@ myManageHook = composeAll <|
 
 type KeyMap = Map.Map (KeyMask, KeySym) (X ())
 
--- TODO: Migrate Awesome Keys, Look at Defaults & Remove Unwanted Ones
 myKeys :: XConfig l -> KeyMap
-myKeys c@XConfig { modMask = modm } = Map.fromList
-  [ -- Run Terminal on <M-Enter>
-    ( ( modm, xK_Return ), terminal c |> spawn )
-  ]
+myKeys c@XConfig { modMask = modm } = Map.fromList $
+    [
+
+    -- Run Terminal on <M-Enter>
+      ( ( modm, xK_Return )
+      , spawn <| terminal c
+      )
+
+    -- Kill Focused Window
+    , ( ( modm .|. shiftMask, xK_c)
+      , kill
+      )
+
+    -- Next / First Layout
+    , ( ( modm, xK_space )
+      , sendMessage NextLayout
+      )
+    , ( ( modm .|. shiftMask, xK_space )
+      , sendMessage FirstLayout
+      )
+
+    -- Change Focus
+    , ( ( modm, xK_j )
+      , windows W.focusDown
+      )
+    , ( ( modm, xK_k )
+      , windows W.focusUp
+      )
+    , ( ( modm, xK_y )
+      , windows W.focusMaster
+      )
+
+    -- Move Clients
+    , ( ( modm .|. shiftMask, xK_j )
+      , windows W.swapDown
+      )
+    , ( ( modm .|. shiftMask, xK_k )
+      , windows W.swapUp
+      )
+    , ( ( modm, xK_BackSpace )
+      , windows W.swapMaster
+      )
+
+    -- Resize Master Area
+    , ( ( modm .|. shiftMask, xK_h )
+      , sendMessage Shrink
+      )
+    , ( ( modm .|. shiftMask, xK_l )
+      , sendMessage Expand
+      )
+
+    -- Modify Master Window Count
+    , ( ( modm .|. shiftMask, xK_u )
+      , sendMessage <| IncMasterN 1
+      )
+    , ( ( modm .|. shiftMask, xK_i )
+      , sendMessage <| IncMasterN (-1)
+      )
+
+    -- Un-Float Window
+    , ( ( modm .|. shiftMask .|. controlMask, xK_space )
+      , withFocused <| windows <. W.sink
+      )
+
+    -- Quit / Restart
+    , ( ( modm .|. shiftMask, xK_q )
+      , io <| exitSuccess
+      )
+    , ( ( modm, xK_q )
+      , spawn "xmonad --recompile && xmonad --restart"
+      )
+
+    ]
+    ++
+    -- Switch / Move to Workspace
+    [ ( ( modm .|. mask, key )
+      , windows <| action index
+      )
+    | (index, key) <- zip (workspaces c) [xK_1 .. xK_9]
+    , (action, mask) <- [ ( W.greedyView, 0 ), ( W.shift, shiftMask ) ]
+    ]
+    ++
+    -- Switch to Left / Center / Right Monitors
+    [ ( ( modm .|. mask, key )
+      , screenWorkspace screen >>= flip whenJust (action .> windows)
+      )
+    | (key, screen) <- zip [xK_e, xK_w, xK_r] [0 ..]    -- middle = screen 0
+    , (action, mask) <- [ ( W.view, 0 ), ( W.shift, shiftMask ) ]
+    ]
 
 
 -- }}}
